38c3ab87520842516487583f95063b57aa95c35b
Forest Gregg <fgregg@uchicago.edu>
2021-02-06 22:34:14 -0500

    fix quadrants

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ function getLocation() {
 }
 
 function cardinalDirection(closest_point, current_x, current_y) {
-  let degrees = Math.atan2(closest_point[1] - current_y, closest_point[0] - current_x) * 180/Math.PI
+  let degrees = Math.atan2(closest_point[1] - current_y, closest_point[0] - current_x) * 180 / Math.PI
   if (degrees < 0) {
     degrees += 360
   }
-  const compass_quadrants = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"]
+  const compass_quadrants = ["E", "NE", "N", "NW", "W", "SW", "S", "SE", "E"]
   return compass_quadrants[Math.round(degrees/45)]
 }
 
@@ -28,21 +28,21 @@ function showPosition(position) {
       const current_y = json.queryGeometry.y
       fetch(mi_url + `&distance=1.0&where=OBJECTID+<>+${local_vegetation.attributes.OBJECTID}`).then(response => response.json()).then(function(json) {
         const nearby_vegetation = json.features
-
         x.innerHTML = `<h1>${local_vegetation.attributes.COVERTYPE}</h1>`
         for (const feature of nearby_vegetation) {
-          let min_distance = Infinity
-          let closest_point = [0.0, 0.0]
-          for (const point of feature.geometry.rings[0]) {
-            const dist = Math.hypot(point[0] - current_x, point[1] - current_y)
-            if (dist < min_distance) {
-              min_distance = dist
-              closest_point = point
+            console.log(feature.attributes.COVERTYPE)
+            let min_distance = Infinity
+            let closest_point = [0.0, 0.0]
+            for (const point of feature.geometry.rings[0]) {
+                const dist = Math.hypot(point[0] - current_x, point[1] - current_y)
+                if (dist < min_distance) {
+                    min_distance = dist
+                    closest_point = point              
+                }
             }
-          }
 
-          x.innerHTML += `<br />${feature.attributes.COVERTYPE}, ${+(min_distance * 0.000621371).toFixed(2)}, ${cardinalDirection(closest_point, current_x, current_y)}`
+            x.innerHTML += `<br />${feature.attributes.COVERTYPE}, ${+(min_distance * 0.000621371).toFixed(2)}, ${cardinalDirection(closest_point, current_x, current_y)}`
         }
-      })
     })
+})
 }
